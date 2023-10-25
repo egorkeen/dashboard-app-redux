5e05b7e1b84a6850867d2f7b43dd1d6f0948dd84
Egor Kirienko <egor.keen@mail.ru>
2023-10-25 16:48:13 +0300

    feat: заключительные правки по redux

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,19 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { JobPosition } from './JobPosition';
-import {selectAllPositions} from 'store/positions/position-selectors';
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { selectFilters } from 'store/filters/filter-selectors';
+import { addFilter } from 'store/filters/filter-actions';
 
 const JobList = () => {
-  const positions = useSelector(selectAllPositions);
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) => selectVisiblePositions(state, currentFilters));
   const dispatch = useDispatch();
 
-  const handleAddFilter = () => {
-
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
   };
 
   return (
     <div className='job-list'>
       {positions.map(item => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition
+          handleAddFilter={handleAddFilter}
+          key={item.id} 
+          {...item} 
+        />
       ))}
     </div>
   )
